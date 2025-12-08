dd184a543152504fea83f2e9a98b35d90a070a88
tennyballs <player130games@gmail.com>
2025-12-07 17:26:38 -0800

    silly things

---
+++ creations/main.js
@@ -0,0 +1,30 @@
+const creations = document.getElementById('creations');
+
+fetch("/data/creations.json")
+    .then(e => e.json())
+    .then(data => {
+        data.forEach(item => {
+
+            if(item.hidden)
+            {
+                return;
+            }
+            
+            const e = document.createElement('div');
+            e.innerHTML = 
+            `
+            <div class="item">
+                <div class="top">
+                    <img class="thumb" src="${item.thumb}" alt="Thumbnail">
+                    <div class="body">
+                        <h2 class="title">${item.title}</h2>
+                        <p class="description">${item.description}</p>
+                        <date class="date">Last Updated on ${new Date(item.lastUpdated).toLocaleDateString()}</date>
+                        ${item.link.length > 0 ? `<a href="${item.link}">Link</a>` : ``}
+                    </div>
+                </div>
+            </div>
+            `
+            creations.appendChild(e.querySelector('.item'))
+        });
+    })
