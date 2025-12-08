fcdea8bdc9c7c115c4164053eea3d4c52ec814c2
tennyballs <player130games@gmail.com>
2025-12-08 04:35:13 -0800

    we savin some memz ton1te :3

--- creations/Rhythm/script/main.js
+++ creations/Rhythm/script/main.js
@@ -1,3 +1,43 @@
 "use strict";
-let settings = new ArrayBuffer(32);
-console.log(new Uint32Array(settings));
+var Settings;
+(function (Settings) {
+    const IDENTITY = 'rhythm/savedata/settings';
+    let data = new ArrayBuffer(4);
+    const U8 = new Uint8Array(data);
+    const values = {
+        volume: {
+            master: 0,
+            sfx: 0,
+            background: 0,
+            music: 0
+        }
+    };
+    function write() {
+        U8[0] = values.volume.master;
+        U8[1] = values.volume.sfx;
+        U8[2] = values.volume.music;
+        U8[3] = values.volume.background;
+        localStorage.setItem(IDENTITY, String.fromCodePoint(...U8));
+    }
+    function load() {
+        const item = localStorage.getItem(IDENTITY);
+        if (!item) {
+            write();
+            load();
+            return;
+        }
+        item.split("").forEach((str, i) => {
+            U8[i] = str.charCodeAt(0);
+        });
+        values.volume.master = U8[0];
+        values.volume.sfx = U8[1];
+        values.volume.music = U8[2];
+        values.volume.background = U8[3];
+    }
+    Settings.load = load;
+    function getValues() {
+        return values;
+    }
+    Settings.getValues = getValues;
+    document.addEventListener('DOMContentLoaded', load);
+})(Settings || (Settings = {}));
--- creations/Rhythm/src/main.ts
+++ creations/Rhythm/src/main.ts
@@ -1,3 +1,56 @@
+namespace Settings {
+    const IDENTITY = 'rhythm/savedata/settings';
+    let data = new ArrayBuffer(4); // alloc(4kb)
+    const U8 = new Uint8Array(data);
 
-let settings = new ArrayBuffer(32);
-console.log(new Uint32Array(settings));
+    const values = {
+        volume: {
+            master: 0,
+            sfx: 0,
+            background: 0, // possibly an unused property
+            music: 0
+        }
+    }
+    
+    function write()
+    {
+        // move all data into the array before saving to disk.
+        // WHEN LOADING PLEASE DO NOT ORDER THIS ANY DIFFERENT.
+        U8[0] = values.volume.master
+        U8[1] = values.volume.sfx
+        U8[2] = values.volume.music
+        U8[3] = values.volume.background
+
+        // save to disk.
+        localStorage.setItem(IDENTITY, String.fromCodePoint(...U8));
+    }
+
+    // we are kinda in a rush rn, gotta push changes before class begins. -- 8/12/2025
+    export function load()
+    {
+        const item = localStorage.getItem(IDENTITY);
+        if(!item) {
+            write();
+            load();
+            return;
+        }
+
+        item.split("").forEach((str, i) => {
+            U8[i] = str.charCodeAt(0)
+        })
+
+
+        values.volume.master = U8[0];
+        values.volume.sfx = U8[1];
+        values.volume.music = U8[2];
+        values.volume.background = U8[3];
+    }
+
+    export function getValues()
+    {
+        return values;
+    }
+
+    // load when the user loads the document
+    document.addEventListener('DOMContentLoaded', load);
+}
